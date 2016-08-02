7fd3854d238526e4bc5b6476d154691a4280b288
Nahun Echeverria Aguilar <nahunecheverria@unitec.edu>
2016-08-01 20:40:57 -0600

    Proyecto Bodega Pineda

---
+++ IS2_BodegaPineda/Startup.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(IS2_BodegaPineda.Startup))]
+namespace IS2_BodegaPineda
+{
+    public partial class Startup {
+        public void Configuration(IAppBuilder app) {
+            ConfigureAuth(app);
+        }
+    }
+}
