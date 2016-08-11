376c58835be195e22d9a024db36512dd6a5d6d52
Nahun Echeverria Aguilar <nahunecheverria@unitec.edu>
2016-08-10 22:32:24 -0600

    Registro de la base de datos al modelo entityFW

---
+++ WebApplicationBodegaPineda/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(WebApplicationBodegaPineda.Startup))]
+namespace WebApplicationBodegaPineda
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
